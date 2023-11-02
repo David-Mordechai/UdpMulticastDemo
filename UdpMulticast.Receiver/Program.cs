0a1c6306316071203577be9a10b43ac7e619de2e
David Mordechai <David.mordechai79@gmail.com>
2023-11-02 11:30:08 +0200

    Add project files.

---
+++ UdpMulticast.Receiver/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+const int port = 12345;
+const string multiCastAddress = "239.0.0.1";
+
+using var udpClient = new UdpClient(port);
+udpClient.JoinMulticastGroup(IPAddress.Parse(multiCastAddress));
+
+var endPoint = new IPEndPoint(IPAddress.Any, port);
+
+while (true)
+{
+    var receivedData = udpClient.Receive(ref endPoint);
+    var message = Encoding.UTF8.GetString(receivedData);
+    Console.WriteLine($"Received: {message}");
+}
---
+++ UdpMulticast.Sender/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+const int port = 12345;
+const string multiCastAddress = "239.0.0.1";
+
+using var udpClient = new UdpClient();
+udpClient.JoinMulticastGroup(IPAddress.Parse(multiCastAddress), 50);
+var endPoint = new IPEndPoint(IPAddress.Parse(multiCastAddress), port);
+
+while (true)
+{
+    const string message = "Hello from sender";
+    var data = Encoding.UTF8.GetBytes(message);
+    udpClient.Send(data, data.Length, endPoint);
+    Console.WriteLine($"Sent: {message}");
+    Thread.Sleep(1000); // Send a message every second
+}
